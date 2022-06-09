49f6968ac41dff09b82c4e291dfa2af03f2d4885
Luan Cardoso <luanccardoso23@gmail.com>
2022-06-09 20:50:42 -0300

    implementando visualização da API no HTML

--- nativo/script.js
+++ nativo/script.js
@@ -1,3 +1,5 @@
+let lista = document.querySelector("#lista")
+
 async function carregarDados(){
     const url = "https://swapi.dev/api/people/?format=json"
     try {
@@ -6,7 +8,23 @@ async function carregarDados(){
     const dados = await resultado.json()
 
         for(elementos of dados.results){
-            console.log(` Oi, sou ${elementos.name} e nasci no ano ${elementos.birth_year}`)
+            //console.log(` Oi, sou ${elementos.name} e nasci no ano ${elementos.birth_year}`)
+            console.log(elementos)
+
+            //Criando elementos HTML dinamicamente
+            /*
+            const itemLista = document.createElement("li")
+            itemLista.classList.add("list-group-item")
+            itemLista.textContent =  `Hi my name is ${elementos.name}, my gender sex is ${elementos.gender} and my hair color is ${elementos.hair_color}`
+            
+
+            
+            
+            lista.appendChild(itemLista)
+            */
+
+            const itemTabela = document.createElement("li")
+            
 
         }
 
